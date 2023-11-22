676c72dbe810700ab09c56e7353802b88c28c6dd
Nikita Dhumal <nikita.dhumal@8848digital.com>
2023-11-22 12:12:02 +0530

    In search select dropdown handle key down implemented

--- components/SearchSelectInputField/SearchSelectInputField.tsx
+++ components/SearchSelectInputField/SearchSelectInputField.tsx
@@ -11,95 +11,77 @@ const SearchSelectInputField = ({
   setStateForDocStatus,
   placeholder,
   className,
-
 }: any) => {
   const inputRef = useRef<any>(null);
   const [showDropdown, setShowDropdown] = useState(false);
   const [selectedIndex, setSelectedIndex] = useState<any>(-1);
-  // const [selectedDropdownValue, setSelectedDropdownValue] = useState();
+  const [scrollIndex, setScrollIndex] = useState(0);
   const [noRecords, setNoRecordsFound] = useState(false);
   const [filterDropdownList, setFilterDropdownList] = useState([]);
-  //const [masterData, setMasterData] = useState<any>();
-
-  console.log('check karigar', karigarData);
-  console.log(typeof [karigarData], 'type ');
-  // useEffect(() => {
-  //   if (karigarData?.length > 0) {
-  //     setMasterData(karigarData);
-  //   }
-  // }, []);
-  // console.log('master state', masterData);
-  const HandleSelectInputField = (e: any) => {
-    console.log('input field11', e.target.value);
-    setShowDropdown(true);
-    setSelectedDropdownValue(e.target.value);
-    const query = e.target.value;
-
-    const UpdatedFilterList: any =
-      karigarData?.length > 0 &&
-      karigarData.filter((item: any) => {
-        return (
-          item.karigar_name?.toLowerCase()?.indexOf(query?.toLowerCase()) !== -1
-        );
-      });
-    setFilterDropdownList(UpdatedFilterList);
-    setNoRecordsFound(true);
-    if (setRecipitData !== undefined) {
-      setRecipitData({ ...recipitData, custom_karigar: selectedDropdownValue });
-    }
-    if (setStateForDocStatus !== undefined) {
-      setStateForDocStatus(true);
-    }
-  };
+  const dropdownRef = useRef<HTMLUListElement>(null);
 
   const handleShowDropdown = () => {
     setShowDropdown(!showDropdown);
     setSelectedIndex(-1);
+    setFilterDropdownList(karigarData)
   };
-  const handleKeyDown = (e:any) => {
-    if (e.key === 'ArrowDown' && !showDropdown) {
-      setShowDropdown(true);
-      setSelectedIndex(-1);
-    } else if (e.key === 'ArrowDown' && showDropdown) {
-      
-      console.log(selectedIndex,'selectedIndex handleKey')
-      setSelectedIndex((prevIndex:any) => (prevIndex + 1 )
-      );
-    } else if (e.key === 'ArrowUp' && showDropdown) {
-      setSelectedIndex((prevIndex:any) => (prevIndex > 0 ? prevIndex - 1 : 0));
-    } else if (e.key === 'Enter' && showDropdown && selectedIndex !== -1) {
-      handleSelectedOption(filterDropdownList[selectedIndex]);
-    }
-  };
-  console.log(selectedIndex,'selectedIndex handleKey')
 
-  const handleSelectedOption = (data: any) => {
-    console.log('dataa11', data.karigar_name);
-    setSelectedDropdownValue(data.karigar_name);
+  const handleSelectedOption = (data: any, i: any) => {
+    setSelectedDropdownValue(data?.karigar_name);
     setShowDropdown(false);
+    setSelectedIndex(i !== undefined ? i : -1);
     if (setRecipitData !== undefined) {
-      setRecipitData({ ...recipitData, custom_karigar: data.karigar_name });
+      setRecipitData({ ...recipitData, custom_karigar: data?.karigar_name });
     }
     if (setStateForDocStatus !== undefined) {
       setStateForDocStatus(true);
     }
   };
-  console.log(selectedDropdownValue, 'selected value');
+
+  const handleDocumentClick = (e: any) => {
+    // Check if the input element itself was clicked
+    if (e?.target !== inputRef?.current && !inputRef?.current?.contains(e.target)) {
+      setShowDropdown(false);
+    }
+  };
+
+  const handleKeyDown = (e: any) => {
+    if (e.key === 'ArrowDown' && !showDropdown) {
+      e.preventDefault();
+      setShowDropdown(true);
+      setSelectedIndex(-1);
+      setFilterDropdownList(karigarData )
+    } else if (e.key === 'ArrowDown' && showDropdown) {
+      // setSelectedIndex((prevIndex: any) => ( prevIndex + 1));
+      setSelectedIndex((prevIndex:any) =>
+          prevIndex < filterDropdownList?.length - 1 ? prevIndex + 1 : prevIndex
+        );
+      setScrollIndex((prevScrollIndex) => Math.min(prevScrollIndex + 1, filterDropdownList?.length - 1));
+    } else if (e.key === 'ArrowUp' && showDropdown) {
+      e.preventDefault();
+      setSelectedIndex((prevIndex: any) => (prevIndex > 0 ? prevIndex - 1 : 0));
+      setScrollIndex((prevScrollIndex) => Math.max(prevScrollIndex - 1, 0));
+    } else if ((e.key === 'Enter' || e.keyCode === 13) && showDropdown && selectedIndex !== -1) {
+      e.preventDefault();
+      handleSelectedOption(filterDropdownList[selectedIndex], selectedIndex);
+    }
+  };
+
   useEffect(() => {
-    const handleDocumentClick = (e: any) => {
-      // Check if the input element itself was clicked
-      if (
-        e?.target !== inputRef?.current &&
-        !inputRef?.current?.contains(e.target)
-      ) {
-        setShowDropdown(false);
-      }
-    };
     document.addEventListener('click', handleDocumentClick);
     return () => {
       document.removeEventListener('click', handleDocumentClick);
     };
   }, []);
+
+  useEffect(() => {
+    if (showDropdown && dropdownRef.current) {
+      const selectedItem = dropdownRef.current.childNodes[selectedIndex] as HTMLElement;
+      if (selectedItem) {
+        selectedItem.scrollIntoView({ block: 'nearest' });
+      }
+    }
+  }, [selectedIndex, showDropdown]);
 
   return (
     <div>
@@ -107,19 +89,36 @@ const SearchSelectInputField = ({
         type="text"
         name="custom_karigar"
         className={className}
-        id="exampleInputEmail1"
         placeholder={placeholder}
         onBlur={() => setShowDropdown(false)}
-        onChange={HandleSelectInputField}
-        onClick={handleShowDropdown}
+        onChange={(e) => {
+          setShowDropdown(true);
+          setSelectedDropdownValue(e.target.value);
+          const query = e.target.value;
+          const updatedFilterList: any =
+            karigarData?.length > 0 &&
+            karigarData.filter((item: any) => {
+              return item.karigar_name?.toLowerCase()?.indexOf(query?.toLowerCase()) !== -1;
+            });
+          setFilterDropdownList(updatedFilterList);
+          setNoRecordsFound(true);
+          if (setRecipitData !== undefined) {
+            setRecipitData({ ...recipitData, custom_karigar: selectedDropdownValue });
+          }
+          if (setStateForDocStatus !== undefined) {
+            setStateForDocStatus(true);
+          }
+          handleKeyDown(e)
+        }}
+        onMouseDown={handleShowDropdown}
         value={selectedDropdownValue}
         defaultValue={defaultValue}
-        onKeyDown={(e)=>handleKeyDown(e)}
+        onKeyDown={handleKeyDown}
         autoComplete="off"
         ref={inputRef}
       />
       {showDropdown && (
-        <ul className=" dropdown-ul-list border">
+        <ul className="dropdown-ul-list border" ref={dropdownRef}>
           {noRecords === false && filterDropdownList?.length === 0 ? (
             <>
               {karigarData?.length > 0 &&
@@ -127,7 +126,7 @@ const SearchSelectInputField = ({
                 karigarData.map((name: any, i: any) => (
                   <li
                     key={i}
-                    onClick={() => handleSelectedOption(name)}
+                    onMouseDown={() => handleSelectedOption(name, i)}
                     className={`dropdown-list ${i === selectedIndex ? 'selected' : ''}`}
                   >
                     {name.karigar_name}
@@ -141,7 +140,7 @@ const SearchSelectInputField = ({
                 filterDropdownList.map((name: any, i: any) => (
                   <li
                     key={i}
-                    onClick={() => handleSelectedOption(name)}
+                    onMouseDown={() => handleSelectedOption(name, i)}
                     className={`dropdown-list ${i === selectedIndex ? 'selected' : ''}`}
                   >
                     {name.karigar_name}
@@ -154,4 +153,5 @@ const SearchSelectInputField = ({
     </div>
   );
 };
+
 export default SearchSelectInputField;
