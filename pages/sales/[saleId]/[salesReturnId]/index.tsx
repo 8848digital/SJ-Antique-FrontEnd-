a48ffa199c2e76c9039f3090343c98ed48656db3
Rushikesh Ahire <rushikesh@8848digital.com>
2024-01-10 13:43:51 +0530

    delete sale return detail pagea

--- pages/sales/[saleId]/[salesReturnId]/index.tsx
+++
@@ -1,8 +0,0 @@
-import DetailsPageSalesReturn from '@/components/Sales/SalesReturns/DetailPageSalesReturn/DetailsPageSalesReturn';
-import React from 'react';
-
-const index = () => {
-  return <>{/* <DetailsPageSalesReturn /> */}</>;
-};
-
-export default index;
