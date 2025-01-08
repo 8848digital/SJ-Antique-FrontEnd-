35e6dc90cb58e24083d7167a50af460d6ca0db74
rushikesh2404 <rushikesh@8848digital.com>
2025-01-08 13:05:04 +0530

    fix: config file changes

--- services/config/api-config.ts
+++ services/config/api-config.ts
@@ -1,6 +1,5 @@
 export const CONSTANTS = {
-  // API_BASE_URL: 'https://shilpiantique.8848digitalerp.com',
-  API_BASE_URL: 'https://stag-shilpiantique.8848digitalerp.com',
+  API_BASE_URL: 'https://shilpiantique.8848digitalerp.com',
   // API_BASE_URL: 'http://192.168.29.54:8000',
 
   // Production Backend URL
